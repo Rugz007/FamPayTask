889e78e0e697c40a497ba4564a09f7a528cbfc87
Rugz007 <dsweb007@gmail.com>
2021-08-06 17:04:20 +0530

    Implemented Multiple API Key support

--- backend/main/admin.py
+++ backend/main/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Video
+from .models import APIKey, Video
 # Register your models here.
 
 class VideoAdmin(admin.ModelAdmin):
+    
     list_display=['title','channel_title']
+class APIKeyAdmin(admin.ModelAdmin):
+    
+    list_display=['key']
 
 admin.site.register(Video,VideoAdmin)
+admin.site.register(APIKey,APIKeyAdmin)
--- backend/main/tasks.py
+++ backend/main/tasks.py
@@ -1,9 +1,10 @@
-from .models import Video
+from .models import APIKey, Video
 import os
 import googleapiclient.discovery
 import googleapiclient.errors
 from celery import shared_task
 import datetime
+from googleapiclient.errors import HttpError
 
 scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
@@ -12,42 +13,59 @@ scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 def fetch_videos():
     api_service_name = "youtube"
     api_version = "v3"
+    api_key = APIKey.objects.all().first()
+    if api_key is None:
+        print("No API Key")
+        return
+    else:
+        print(api_key)
     youtube = googleapiclient.discovery.build(
         api_service_name,
         api_version,
-        developerKey="AIzaSyBeZGVY1liN5gTtbmvBvudNlZogyWqhgiI",
+        developerKey=api_key.key,
     )
     video = Video.objects.all().order_by("-published_at").first()
     if video is not None:
         published_after = video.published_at.replace(tzinfo=None)
     else:
-        published_after = datetime.datetime.utcnow() - datetime.timedelta(minutes=10)
+        published_after = datetime.datetime.utcnow() - datetime.timedelta(minutes=30)
     published_after = published_after.isoformat("T") + "Z"
-    try:
-        response = youtube.search().list(
-            part="snippet",
-            maxResults=25,
-            q="cooking",
-            type="video",
-            publishedAfter=published_after,
-        ).execute()
-        if len(response["items"]) == 0:
-            return
-        for video in response["items"]:
-            try:
-                video = Video.objects.get(video_id=video["id"]["videoId"])
-            except:
-                video = Video(
-                    video_id=video["id"]["videoId"],
-                    title=video["snippet"]["title"],
-                    description=video["snippet"]["description"],
-                    channel_title=video["snippet"]["channelTitle"],
-                    thumbnail_url=video["snippet"]["thumbnails"]["default"]["url"],
-                    published_at=video["snippet"]["publishTime"],
-                )
-                video.save()
-    except:
-        print("Error")
-    # if response["nextPageToken"]:
-    #     pass
+    next_page_token = None
+    while True:
+        try:
+                response = youtube.search().list(
+                    part="snippet",
+                    maxResults=25,
+                    q="gaming",
+                    type="video",
+                    publishedAfter=published_after,
+                    pageToken=next_page_token
+                ).execute()
+                if len(response["items"]) == 0:
+                    return
+                for video in response["items"]:
+                    print("Fetching")
+                    try:
+                        video = Video.objects.get(video_id=video["id"]["videoId"])
+                    except:
+                        video = Video(
+                            video_id=video["id"]["videoId"],
+                            title=video["snippet"]["title"],
+                            description=video["snippet"]["description"],
+                            channel_title=video["snippet"]["channelTitle"],
+                            thumbnail_url=video["snippet"]["thumbnails"]["default"]["url"],
+                            published_at=video["snippet"]["publishTime"],
+                        )
+                        video.save()
+                if "nextPageToken" in response:
+                    next_page_token = response["nextPageToken"]
+                else:
+                    break
+        except HttpError as e:
+            if e.resp['status'] == '403':
+                print("error")
+                if APIKey.objects.all().count() > 1:
+                    api_key.delete()
+                return
+        
 
